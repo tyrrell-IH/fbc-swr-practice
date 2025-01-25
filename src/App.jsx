5d68cc6ef5fc45574e918b3fce360c9f4ee2be79
tyrrell-IH <gotothertm@gmail.com>
2025-01-25 23:42:14 +0900

    APIコール時の、コールが失敗した場合、レスポンスが返ってくる待ち状態の場合、レスポンスが返ってきた場合の表示をSWRで実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import useSWR from "swr";
 import "./App.css";
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
 
-  const [status, setStatus] = useState("");
+  const fetcher = ([url, headers]) =>
+    fetch(url, { headers })
+      .then((res) => res.json())
+      .then((json) => json.description);
 
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
+  const { data, error, isLoading } = useSWR([url, headers], fetcher);
 
-  return <>{status && <p>Status : {status}</p>}</>;
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return <p>Status: {data}</p>;
 }
 
 export default App;
